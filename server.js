db1071f28cc751f416703c7143cb03ea14d177f3
pavani <pavani@gamil.com>
2024-02-01 00:03:34 +0530

    first commit

---
+++ server.js
@@ -0,0 +1,61 @@
+import express from 'express';
+import colors from  'colors';
+import dotenv from 'dotenv';
+import morgan from 'morgan';
+import connectDB from "./config/db.js";
+import authRoute from "./routes/authRoute.js";
+import categoryRoutes from './routes/CategoryRoutes.js';
+import productRoutes from "./routes/productRoutes.js";
+import cors from 'cors'
+import path from 'path'
+import { fileURLToPath } from 'url';
+//config env
+dotenv.config();
+
+//db config
+connectDB();
+
+//esmodulefix
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+// rest object
+const app = express();
+
+//middlewares
+app.use(cors());
+app.use(express.json());
+app.use(morgan('dev'));
+app.use(express.static(path.join(__dirname, './frontend/build')))
+
+//routes
+app.use("/api/v1/auth", authRoute);
+app.use("/api/v1/category", categoryRoutes);
+app.use("/api/v1/product", productRoutes);
+
+// rest api
+app.use('*',function(req,res){
+  res.sendFile(path.join(__dirname,'./frontend/build/index.html'));
+})
+
+//port
+const PORT = process.env.PORT || 8000;
+
+
+app.get('/weather', async (req, res) => {
+    const { name } = req.name;
+    const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${name}&appid=e990d730142f1741391b7b5d71bad39c&units=metric`;
+    
+    try {
+      const response = await axios.get(apiUrl);
+      res.json(response.data);
+    } catch (error) {
+      res.status(error.response?.status || 500).json(error.response?.data || { error: 'Internal Server Error' });
+    }
+  });
+
+//run listen
+app.listen(PORT, () => {
+    console.log(`Server Running on ${process.env.DEV_MODE} mode on port  ${PORT}`.bgCyan.white);
+});
+
